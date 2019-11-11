729ffaa2dcefb9a7b7d8ff1bd8aea8a7fa29b884
suresh-kumara-gist <kumara.suresh25@gmail.com>
2019-11-11 15:40:32 +0530

    complete watch shop

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+// lib/utils/colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = const Color(0xFF353434);
+  static const primaryLightColor = const Color(0xFFEEEDED);
+  static const primaryDarkColor = const Color(0xFFE2E1E1);
+
+  static const statusBarColor = Colors.white;
+  static const appBarColor = primaryDarkColor;
+  static const scaffoldColor = primaryDarkColor;
+}
